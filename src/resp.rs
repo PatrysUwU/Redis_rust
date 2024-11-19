6ba579905c175d32febddf061514e17763d34707
tzgfryziu <patryk.fryzlewicz@gmail.com>
2024-11-19 13:46:42 +0100

    Expiry

--- src/resp.rs
+++ src/resp.rs
@@ -1,16 +1,20 @@
 mod lexer;
 
 use std::collections::HashMap;
+use std::num::ParseIntError;
+use std::time::Duration;
 use bytes::BytesMut;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 use anyhow::{anyhow, Result};
+use tokio::time::Instant;
 use lexer::Lexer;
 #[derive(Debug, Clone)]
 pub enum Value {
     SimpleString(String),
     BulkString(String),
     Array(Vec<Value>),
+    NullBulkString,
 }
 
 impl Value {
@@ -21,6 +25,7 @@ impl Value {
             Value::Array(s) => {
                 format!("arejek {:?}", s)
             }
+            Value::NullBulkString => { "$-1\r\n".to_string() }
             _ => panic!("Unsupported value to serialize!")
         }
     }
@@ -28,15 +33,53 @@ impl Value {
         match self {
             Value::SimpleString(x) => { x.to_string() }
             Value::BulkString(x) => { x.to_string() }
-            Value::Array(x) => { panic!("fsdfasfsda") }
+            _ => { panic!("fsdfasfsda") }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Item {
+    val: Value,
+    time_created: Instant,
+    expiry_time: Option<Instant>,
+}
+
+impl Item {
+    fn new(val: String, exp: Option<u64>) -> Self {
+        let exp = match exp {
+            None => {
+                None
+            }
+            Some(x) => {
+                Some(Instant::now() + Duration::from_millis(x))
+            }
+        };
+        Item {
+            val: Value::BulkString(val),
+            time_created: Instant::now(),
+            expiry_time: exp,
+        }
+    }
+
+    fn is_expired(&self) -> bool {
+        if let Some(x) = self.expiry_time {
+            if Instant::now() > x {
+                true
+            } else {
+                false
+            }
+        } else {
+            false
         }
     }
 }
 pub struct RespHandler {
     stream: TcpStream,
     lexer: Lexer,
-    storage: HashMap<String, Value>,
+    storage: HashMap<String, Item>,
 }
+
 
 impl RespHandler {
     pub fn new(stream: TcpStream) -> Self {
@@ -57,7 +100,6 @@ impl RespHandler {
                     panic!("Command must be a bulk string!");
                 };
                 let args: Vec<Value> = arr.into_iter().skip(1).collect();
-                println!("{:?}", args);
                 match &command as &str {
                     "PING" => {
                         Ok(Some(Value::SimpleString(String::from("PONG"))))
@@ -66,11 +108,24 @@ impl RespHandler {
                         Ok(Some(self.echo(args)?))
                     }
                     "SET" => {
-                        self.set(args)?;
-                        Ok(Some(Value::SimpleString(String::from("OK"))))
+                        match self.set(args) {
+                            Ok(_) => {
+                                Ok(Some(Value::SimpleString(String::from("OK"))))
+                            }
+                            Err(e) => {
+                                Ok(Some(Value::SimpleString(String::from(format!("{}", e)))))
+                            }
+                        }
                     }
                     "GET" => {
-                        self.get(args)
+                        match self.get(args) {
+                            Ok(x) => {
+                                Ok(x)
+                            }
+                            Err(e) => {
+                                Ok(Some(Value::SimpleString(String::from(format!("{}", e)))))
+                            }
+                        }
                     }
                     _ => { Ok(Some(Value::SimpleString(String::from("cos nie tak kolego")))) }
                 }
@@ -85,8 +140,6 @@ impl RespHandler {
 
     pub async fn write_value(&mut self, v: Value) -> Result<()> {
         self.stream.write(v.serialize().as_bytes()).await?;
-        // println!("{}", v.tostring());
-        // self.stream.write(v.tostring().as_bytes()).await?;
         Ok(())
     }
 
@@ -98,20 +151,93 @@ impl RespHandler {
         }
     }
 
+    fn extract_arguments(&self, args: &Vec<Value>) -> Option<HashMap<String, String>> {
+        let is_odd = args.len() % 2;
+        let mut result = HashMap::new();
+        for i in (0..(args.len() - is_odd)).step_by(2) {
+            let k = if let Value::BulkString(x) = &args[i] {
+                x.to_ascii_uppercase()
+            } else {
+                panic!("Arg not bulkstring")
+            };
+            let v = if let Value::BulkString(x) = &args[i + 1] {
+                x.to_ascii_uppercase()
+            } else {
+                panic!("Arg not bulkstring")
+            };
+            result.insert(k.to_string(), v.to_string());
+        }
+        println!("{:?}", result);
+        if result.is_empty() {
+            return None;
+        };
+        Some(result)
+    }
+
     fn set(&mut self, args: Vec<Value>) -> Result<()> {
-        if let Value::BulkString(x) = &args[0] {
-            self.storage.insert(x.to_string(), args[1].clone());
-            Ok(())
+        if args.get(0).is_none() || args.get(1).is_none() {
+            Err(anyhow!("Not enough arguments in set"))
         } else {
-            Err(anyhow!("Passed nonbulkstring to set"))
+            if let Value::BulkString(x) = &args.clone()[0] {
+                if let Value::BulkString(y) = &args.clone()[1] {
+                    let args = self.extract_arguments(&args.into_iter().skip(2).collect());
+                    match args {
+                        Some(args) => {
+                            match args.get("PX") {
+                                None => {
+                                    println!("no atributes item stored");
+                                    self.storage.insert(x.to_string(), Item::new(y.to_string(), None));
+                                }
+                                Some(duration) => {
+                                    let t = match duration.clone().parse::<u64>() {
+                                        Ok(t) => {
+                                            let test = Item::new(y.to_string(), Some(t));
+                                            println!("{:?}", test);
+                                            self.storage.insert(x.to_string(), test);
+                                        }
+                                        Err(e) => {
+                                            return Err(anyhow!("{}",e));
+                                        }
+                                    };
+                                }
+                            }
+                        }
+                        None => {
+                            self.storage.insert(x.to_string(), Item::new(y.to_string(), None));
+                        }
+                    }
+                    println!("{:?}", self.storage);
+                    Ok(())
+                } else {
+                    Err(anyhow!("Didn't pass enough arguments to set"))
+                }
+            } else {
+                Err(anyhow!("Passed nonbulkstring to set"))
+            }
         }
     }
 
     fn get(&self, args: Vec<Value>) -> Result<Option<Value>> {
-        if let Value::BulkString(x) = &args[0] {
-            Ok(self.storage.get(x).cloned())
+        if args.get(0).is_none() {
+            Err(anyhow!("Not enough arguments in get"))
         } else {
-            Err(anyhow!("Passed nonbulkstring to get"))
+            if let Value::BulkString(x) = &args[0] {
+                let item = self.storage.get(x);
+                match item {
+                    None => {
+                        Err(anyhow!("Didn't find {} in database",x))
+                    }
+                    Some(x) => {
+                        if !x.is_expired() {
+                            Ok(Some(x.val.clone()))
+                        } else {
+                            Ok(Some(Value::NullBulkString))
+                        }
+                    }
+                }
+            } else {
+                Err(anyhow!("Passed nonbulkstring to get"))
+            }
         }
     }
 }
--- src/resp/lexer.rs
+++ src/resp/lexer.rs
@@ -19,17 +19,23 @@ impl Lexer {
     async fn update_buffer(&mut self, stream: &mut TcpStream) -> Result<Option<()>> {
         println!("Updating buffer");
         self.buffer.clear();
-        let read_bytes_count = stream.read_buf(&mut self.buffer).await?;
-        if read_bytes_count == 0 {
-            return Ok(None);
+        match stream.read_buf(&mut self.buffer).await {
+            Ok(read_butes_count) => {
+                if read_butes_count == 0 {
+                    return Ok(None);
+                }
+                self.read_position = 0;
+                Ok(Some(()))
+            }
+
+            Err(e) => {
+                return Ok(None)
+            }
         }
-        self.read_position = 0;
-        Ok(Some(()))
     }
 
     pub async fn read_value(&mut self, stream: &mut TcpStream) -> Result<Option<Value>> {
         if let Some(_) = self.update_buffer(stream).await? {
-            println!("Reading value");
             if let Ok(mess) = self.consume() {
                 Ok(Some(self.parse_message(&mess)?))
             } else {
@@ -56,51 +62,31 @@ impl Lexer {
                 break;
             }
             result_string.push(self.consume()?.to_owned() as char);
-            println!("{}", result_string);
         }
         self.skip_crlf();
-        if let Some(x) = self.peek() {
-            println!("Where Simple String ended: {}", x.to_ascii_lowercase());
-        } else {
-            println!("Simple string ended outside the buffer!")
-        }
         Ok(Value::SimpleString(result_string))
     }
 
     fn handle_bulk_string(&mut self) -> Result<Value> {
         let mut result_string = String::new();
         let bulk_string_length: usize = self.consume_int()?;
-        println!("{}", bulk_string_length);
         self.skip_crlf();
         for _ in 0..bulk_string_length {
             result_string.push(self.consume()?.to_owned() as char);
-            println!("{}", result_string);
         }
         self.skip_crlf();
-        if let Some(x) = self.peek() {
-            println!("Where Bulk String ended: {}", x.to_ascii_lowercase());
-        } else {
-            println!("Bulk string ended outside the buffer!")
-        }
         Ok(Value::BulkString(result_string))
     }
 
     fn handle_array(&mut self) -> Result<Value> {
         let mut result_vec: Vec<Value> = Vec::new();
         let arr_length: usize = self.consume_int()?;
-        println!("{}", arr_length);
         self.skip_crlf();
         for _ in 0..arr_length {
             let consumed = self.consume()?;
             result_vec.push(self.parse_message(&consumed)?);
-            println!("{:?}", result_vec);
         }
-        if let Some(x) = self.peek() {
-            println!("Where array ended: {}", x.to_ascii_lowercase());
-        } else {
-            println!("Array ended outside the buffer!")
-        }
-
+        println!("{:?}", result_vec);
         Ok(Value::Array(result_vec))
     }
     fn consume_int(&mut self) -> Result<usize> {
@@ -142,10 +128,7 @@ impl Lexer {
     }
 
     fn is_crlf_next(&self) -> Result<bool> {
-        // println!("checking crlf{}{}",self.read_position,self.read_position+1);
-
         if self.buffer[self.read_position] == b'\r' && self.buffer[self.read_position + 1] == b'\n' {
-            // if self.buffer[self.read_position] == b'\\' && self.buffer[self.read_position + 1] == b'r' && self.buffer[self.read_position + 2] == b'\\' && self.buffer[self.read_position + 3] == b'n' {
             return Ok(true);
         }
         Ok(false)
